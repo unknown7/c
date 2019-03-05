e56fdfb9ec05b3923498f763bc3bc3e6765c15c0
unknown7 <unknown7@live.cn>
2019-03-05 11:22:11 +0800

    Signed-off-by: unknown7 <unknown7@live.cn>

--- 初始化数组.c
+++ 初始化数组.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 int main() {
-	int arr1[10] = {2};
+	int arr1[10] = {2, [5] = 6};
 	const int SIZE1 = sizeof(arr1) / sizeof(arr1[0]);
 	printf("sizeof(arr1)=%d\n", SIZE1);
-	for (int i = 0; i < SIZE1; i++) {
+	int i;
+	for (i = 0; i < SIZE1; i++) {
 		printf("arr1[%d]=%d\n", i, arr1[i]);
 	}
-	
-	/**
-		用下面方法初始化数组时，只能从零开始有序初始化，估计是编译器版本不同导致 
-	*/
-	int arr2[10] = {[0]=3,6,[2]=7};
+
+	int arr2[10] = {[0]=3,6,[4]=7};
 	const int SIZE2 = sizeof(arr2) / sizeof(arr2[0]);
 	printf("sizeof(arr2)=%d\n", SIZE2);
-	for (int i = 0; i < SIZE2; i++) {
-		printf("arr2[%d]=%d\n", i, arr2[i]);
+	int j;
+	for (j = 0; j < SIZE2; j++) {
+		printf("arr2[%d]=%d\n", j, arr2[j]);
 	}
 	
 	return 0;
