19681cd9986d666b68909077e5964d41e4cb8edd
weifeng <unknown7@live.cn>
2019-03-07 19:35:39 +0800

    Update and rename 构造素数表2.cpp to 构造素数表2.c

--- 构造素数表2.c
+++ 构造素数表2.c
@@ -3,17 +3,19 @@
 int main() {
 	const int SIZE = 20;
 	int primes[SIZE];
-	for (int i = 0; i < SIZE; i++) {
+	int i;
+	for (i = 0; i < SIZE; i++) {
 		primes[i] = 1;
 	}
-	for (int i = 2; i < SIZE; i++) {
+	for (i = 2; i < SIZE; i++) {
 		if (primes[i]) {
-			for (int j = 2; i * j < SIZE; j++) {
+			int j;
+			for (j = 2; i * j < SIZE; j++) {
 				primes[i * j] = 0;
 			}
 		}
 	}
-	for (int i = 2; i < SIZE; i++) {
+	for (i = 2; i < SIZE; i++) {
 		if (primes[i]) {
 			printf("%d\t", i);
 		}
