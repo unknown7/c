e32ba4e1f37f0a8bc51da92d12b0f3c70129ecf8
unknown7 <unknown7@live.cn>
2019-03-07 13:22:25 +0800

    Signed-off-by: unknown7 <unknown7@live.cn>

---
+++ LinkedStack.h
@@ -0,0 +1,15 @@
+#ifndef _LinkedStack_H_
+#define _LinkedStack_H_
+typedef struct _node {
+	int value;
+	struct _node *prev;
+	struct _node *next;
+} Node;
+typedef struct {
+	int size;
+	struct _node *cur;
+} LinkedStack;
+LinkedStack create();
+void push(LinkedStack *s, int x);
+int pop(LinkedStack *s);
+#endif
