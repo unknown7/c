b149436e0d01baed671d4fcf2c525510a21f1c14
unknown7 <unknown7@live.cn>
2019-03-07 22:50:37 +0800

    Signed-off-by: unknown7 <unknown7@live.cn>

--- 最大公约数.c
+++ 最大公约数.c
@@ -3,12 +3,13 @@ int main() {
 	int a, b;
 	scanf("%d %d", &a, &b);
 	int max = 0;
-	for (int i = 2; i < a; i++) {
-		if (a % i == 0 && i < b && b % i == 0) {
+	int i;
+	for (i = 2; i <= a; i++) {
+		if (a % i == 0 && i <= b && b % i == 0) {
 			max = i;
 		}
 	}
-	printf("脳卯麓贸鹿芦脭录脢媒脦陋拢潞%d", max);
+	printf("最大公约数为%d", max);
 	return 0;
 }
 
