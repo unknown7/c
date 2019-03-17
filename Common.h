fef7c84dc8293d7203f35180a053e82123d99c67
unknown7 <unknown7@live.cn>
2019-03-17 21:01:57 +0800

    Signed-off-by: unknown7 <unknown7@live.cn>

--- Common.h
+++ Common.h
@@ -17,6 +17,13 @@ int* createArray(int size) {
 	}
 	return a;
 }
+void printArray(int *a, int size) {
+	int i;
+	for (i = 0; i < size; i++) {
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+}
 void sort(int *a, int n);
 #endif
 
