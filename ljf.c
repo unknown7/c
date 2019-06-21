317e1c5bc4958c48a8abf58531dc3e1479f33bac
unknown7 <unknown7@live.cn>
2019-06-22 00:26:00 +0800

    Signed-off-by: unknown7 <unknown7@live.cn>

--- ljf.c
+++ ljf.c
@@ -9,7 +9,7 @@ int main() {
 	for (i = m; i <= n; i++) {
 		int isPrime = 1;
 		int k;
-		for (k = 2; k < i; k++) {
+		for (k = 2; k < i-1; k++) {
 			if (i % k == 0) {
 				isPrime = 0;
 				break;
