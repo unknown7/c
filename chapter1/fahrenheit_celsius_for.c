8b683bc1cff20573fd63ea0af7268ac4790f52e6
unknown7 <unknown7@live.cn>
2021-07-02 11:16:29 +0800

    .o after..

---
+++ chapter1/fahrenheit_celsius_for.c
@@ -0,0 +1,37 @@
+/**
+ * fahrenheit		celsius
+ * 1 					-17
+ * 20 					-6
+ * 40 					4
+ * 60 					15
+ * 80 					26
+ * 100 					37
+ * 120 					48
+ * 140 					60
+ * 160 					71
+ * 180 					82
+ * 200 					93
+ * 220 					104
+ * 240 					115
+ * 260 					126
+ * 280 					137
+ * 300 					148
+ * 
+ * formula:°C=(5/9)(°F-32)
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	float fahrenheit, celsius;
+	float lower, upper, step;
+	lower = 0;
+	upper = 300;
+	step = 20;
+	for (fahrenheit = lower; fahrenheit <= upper; fahrenheit+=step)
+	{
+		celsius = (5.0 / 9.0) * (fahrenheit - 32.0);
+		printf("fahrenheit:%3.0f\tcelsius:%7.2f\n", fahrenheit, celsius);
+	}
+}
