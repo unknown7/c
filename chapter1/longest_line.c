8f399e751ea2c128bd8b6111927d8ec5827a4a1f
unknown7 <unknown7@live.cn>
2021-07-05 17:00:48 +0800

    c

--- chapter1/longest_line.c
+++ chapter1/longest_line.c
@@ -2,47 +2,50 @@
 
 #define MAX 100
 
-int get_line(char a[]);
-void copy(char from[], char to[]);
+int get_line(void);
+void copy(void);
+char line[MAX], longest_line[MAX];
 
 int main()
 {
-	char line[MAX], longest_line[MAX];
+	extern char longest_line[];
 	int max, cur;
 
 	max = 0;
-	while ((cur = get_line(line)) > 1)
+	while ((cur = get_line()) > 1)
 		if (cur > max) {
 			max = cur;
-			copy(line, longest_line);
+			copy();
 		}
 
 	printf("longest:%s\n", longest_line);
 	return 0;
 }
 
-int get_line(char a[])
+int get_line(void)
 {
+	extern char line[];
 	int c, i;
 	i = 0;
 	while ((c = getchar()) != EOF && i + 1 < MAX) {
 		if (c != '\n')
-			a[i++] = c;
+			line[i++] = c;
 		else {
-			a[i++] = '\n';
+			line[i++] = '\n';
 			break;
 		}
 	}
-	a[i++] = '\0';
+	line[i++] = '\0';
 	return i;
 }
 
-void copy(char from[], char to[])
+void copy(void)
 {
+	extern char line[], longest_line[];
 	int i;
 	i = 0;
-	while (from[i] != '\0') {
-		to[i] = from[i];
+	while (line[i] != '\0') {
+		longest_line[i] = line[i];
 		i++;
 	}
 }
